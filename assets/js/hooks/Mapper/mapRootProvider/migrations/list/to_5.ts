de761ed09778bd4395987d16b83f31f7cde2d727
szesciennageometria <>
2026-09-05 14:26:30 +0200

    attempt on paste-bug fix created due to server latency on mutiple pastes

--- assets/js/hooks/Mapper/mapRootProvider/migrations/list/to_5.ts
+++ assets/js/hooks/Mapper/mapRootProvider/migrations/list/to_5.ts
@@ -3,10 +3,18 @@ import { SIGNATURES_GLOWINGROWS_TIMING } from '@/hooks/Mapper/constants/signatur
 export const to_5: MigrationStructure = {
   to: 5,
   up: (prev: any) => {
- const signatureSettings = prev?.signatures || {};
+    const interfaceSettings = prev?.interface || {};
+    const signatureSettings = prev?.signatures || {};
 
     return {
       ...prev,
+      interface: {
+        ...interfaceSettings,
+        //if this is right sould fix "unidentified" parameter pushed to default value of "false" for previously set interfacesetting ?? or am i geting this wrong ? RFC...
+        show_animated_border: interfaceSettings.show_animated_border ?? false,
+        show_animated_outline: interfaceSettings.show_animated_outline ?? false,
+        disable_animated_outlineborder: interfaceSettings.disable_animated_outlineborder ?? false,
+      },
       signatures: {
         ...signatureSettings,
         glowingrows_timing: signatureSettings.glowingrows_timing ?? SIGNATURES_GLOWINGROWS_TIMING.GLOWDEFAULT,
