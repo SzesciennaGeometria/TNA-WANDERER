7af26d6b00d2cfb6cc4c75ed2ece7bfb95b3230d
szesciennageometria <>
2026-09-25 18:55:35 +0200

    verssion info

--- assets/js/hooks/Mapper/components/topbar/Topbar.tsx
+++ assets/js/hooks/Mapper/components/topbar/Topbar.tsx
@@ -1,13 +1,13 @@
-import { sortOnlineFunc } from '@/hooks/Mapper/components/hooks/useGetOwnOnlineCharacters.ts';
-import { useMapRootState } from '@/hooks/Mapper/mapRootProvider';
-import { WithChildren } from '@/hooks/Mapper/types/common.ts';
+import {sortOnlineFunc} from '@/hooks/Mapper/components/hooks/useGetOwnOnlineCharacters.ts';
+import {useMapRootState} from '@/hooks/Mapper/mapRootProvider';
+import {WithChildren} from '@/hooks/Mapper/types/common.ts';
 import clsx from 'clsx';
-import { useMemo } from 'react';
-import { Characters } from '../characters/Characters';
+import {useMemo} from 'react';
+import {Characters} from '../characters/Characters';
 
-const Topbar = ({ children }: WithChildren) => {
+const Topbar = ({children}: WithChildren) => {
   const {
-    data: { characters, userCharacters },
+    data: {characters, userCharacters},
   } = useMapRootState();
 
   const charsToShow = useMemo(() => {
@@ -17,15 +17,19 @@ const Topbar = ({ children }: WithChildren) => {
   return (
     <nav
       className={clsx(
-        'px-2 flex items-center justify-center min-w-0 h-12 pointer-events-auto',
+        'relative px-2 flex items-center justify-center min-w-0 h-12 pointer-events-auto',
         'border-b border-stone-800 bg-gray-800 bg-opacity-5',
         'bg-opacity-70 bg-neutral-900',
       )}
     >
+      {}
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-neutral-200 pointer-events-none">
+        TNA-wanderer_v260904-6
+      </div>
       <span className="flex-1"></span>
       <span className="mr-2"></span>
       <div className="flex gap-1 items-center">
-        <Characters data={charsToShow} />
+        <Characters data={charsToShow}/>
       </div>
 
       {children}
